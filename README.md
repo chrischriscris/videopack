eae89f9c8239e57b7cad0b1688f951b7f90e2d0c
chrischriscris <18-10892@usb.ve>
2024-09-03 23:37:16 -0400

    starting point

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Packs a directory of songs and a cover into a full video, spitting out the timestamps in the process.
 
 Made to facilitate the upload of full albums into YouTube.
+
+## Command used:
+
+- To trim the silence out of the tracks:
+ffmpeg -i input.mp3 -af "silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,aformat=dblp,areverse,silenceremove=start_periods=1:start_duration=1:start_threshold=-60dB:detection=peak,aformat=dblp,areverse" output.flac
+
+- To concatenate the tracks:
+
+- To create a video with a given cover image:
+
---
+++ videopack.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import sys
+from typing import List
+
+import ffmpeg
+
+
+def errprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="videopack",
+        description="concatenates all video files in a directory and converts it into a full video",
+    )
+
+    parser.add_argument("dirname")
+    parser.add_argument("-c", "--cover")
+    parser.add_argument("-d", "--disable-cover", action="store_true")
+
+    return parser.parse_args()
+
+
+def concat_music_files(files: List[str]):
+    """Takes a list of filenames and concats all music files into a single file"""
+    ffmpeg.concat()
+    pass
+
+def main():
+    args = get_args()
+    dir = args.dirname
+    if not os.path.isdir(dir):
+        errprint("Error: The given path is not a directory")
+        sys.exit(1)
+
+    files = os.listdir(dir)
+    if len(files) == 0:
+        errprint("Error: The given path has no files")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
