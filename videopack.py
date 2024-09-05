eaebbee2142a34bed50413812371c9a71e62d365
chrischriscris <18-10892@usb.ve>
2024-09-05 11:24:54 -0400

    some progress

--- videopack.py
+++ videopack.py
@@ -4,8 +4,12 @@ import argparse
 import os
 import sys
 from typing import List
+import tempfile
+import time
 
 import ffmpeg
+
+MUSIC_FILE_EXTENSIONS = [".flac", ".mp3", ".m4a"]
 
 
 def errprint(*args, **kwargs):
@@ -56,8 +60,34 @@ def trim_silence(track_filename: str):
 
 def concat_music_files(files: List[str]):
     """Takes a list of filenames and concats all music files into a single file"""
-    ffmpeg.concat()
-    pass
+    inputs = [ffmpeg.input(f) for f in files]
+    ffmpeg.concat(*inputs, a=1).output("output.flac").run()
+    # with tempfile.NamedTemporaryFile() as tmp:
+    #     for path in files:
+    #         tmp.write(str.encode(f"file '{path}'\n"))
+    #     tmp.seek(0)
+
+    #     print(tmp.read())
+    #     ffmpeg.input(tmp.name, format='concat', safe=0).output("output.flac", c='copy').run()
+
+
+
+
+def listdir_absolute(directory: str) -> List[str]:
+    dirs = []
+    for dirpath, _, filenames in os.walk(directory):
+        for f in filenames:
+            dirs.append(os.path.abspath(os.path.join(dirpath, f)))
+
+    return dirs
+
+
+def is_music_file(filename: str) -> bool:
+    for extension in MUSIC_FILE_EXTENSIONS:
+        if filename.endswith(extension):
+            return True
+
+    return False
 
 
 def main():
@@ -67,12 +97,16 @@ def main():
         errprint("Error: The given path is not a directory")
         sys.exit(1)
 
-    files = os.listdir(dir)
+    files = listdir_absolute(dir)
     if len(files) == 0:
         errprint("Error: The given path has no files")
         sys.exit(1)
 
+    music_files = list(sorted(filter(is_music_file, files)))
+
+    # TODO: Check if there are music files
+    concat_music_files(music_files)
+
 
 if __name__ == "__main__":
-    # main()
-    trim_silence("testdir/01. La Sambainina.flac")
+    main()
